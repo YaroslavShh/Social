840404d489087a85a93076521b93bb7e131c3da2
YaroslavShh <yaroslav.shamov@list.ru>
2023-09-25 22:13:40 +0300

    commit 1

---
+++ src/component/Technologies/Technologies.tsx
@@ -0,0 +1,9 @@
+import Main from "./main.jpg"
+import tech from "./tech.module.css"
+export function Technologies() {
+    return (
+        <div className={tech.container}>
+            <img src={Main} alt="main"/>
+        </div>
+    )
+}
